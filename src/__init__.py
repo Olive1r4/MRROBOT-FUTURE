20fa22a26e3127e8843e918cfaee11686972d2b5
Fernando Oliveira <fernandooliveira@live.com>
2026-01-22 08:08:26 -0300

    ...

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Bot de Scalping para Binance Futures
-Versão: 1.0.0
-"""
-
-__version__ = "1.0.0"
-__author__ = "MRROBOT-FUTURE"
-__description__ = "Bot de Scalping automatizado para Binance Futures com análise técnica avançada"
